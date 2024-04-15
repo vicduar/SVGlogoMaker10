6b74ae3f894fc1055bddcc6e2ee8a15eb0cebc73
unknown <vicduar@gmail.com>
2024-04-14 21:07:27 -0600

    second

---
+++ Main/index.js
@@ -0,0 +1,48 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const SVG=require('./lib/svg.js');
+const{Circle,Triangle,Square}=require('./lib/shapes.js');
+
+inquirer
+  .prompt([
+    {
+      type: 'list',
+      name: 'shape',
+      message: 'Choose a shape',
+      choices:['Circle','Triangle','Square']
+    },
+    {
+      type: 'input',
+      name: 'text',
+      message: 'What text do you want on the logo?',
+    },
+    {
+      type: 'input',
+      name: 'textColor',
+      message: 'What is the color of the text?',
+    },
+    {
+      type: 'input',
+      name: 'shapeColor',
+      message: 'What is the color for the shape?',
+    },
+
+  ])
+  .then((answers) => {
+    let shape;
+    if(answers.shape==='Circle'){
+        shape=new Circle()
+
+    } else if(answers.shape==='Triangle'){
+        shape=new Triangle()
+    } else{shape=new Square()}
+
+    shape.setColor(answers.shapeColor)
+    const svg=new SVG()
+    svg.setText(answers.text,answers.textColor)
+    svg.setShape(shape)
+
+    fs.writeFile('logo.svg', svg.render(), (err) =>
+      err ? console.log(err) : console.log('Successfully created Logo!')
+    );
+  });
---
+++ Main/lib/svg.js
@@ -0,0 +1,15 @@
+class SVG {
+  constructor() {
+    this.textEl = "";
+    this.shapeEl = "";
+  }
+  render() {
+    return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">${this.shapeEl}${this.textEl}</svg>`;
+  }
+  setText(text, color) {
+    this.textEl = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`;
+  }
+    setShape(shape){this.shapeEl=shape.render()}
+}
+    module.exports=SVG
+    
